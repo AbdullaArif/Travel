681ebed02781929ac8adb0db89e51bca2de4f409
AbdullaArif <arifabdulla034@gmail.com>
2024-08-17 20:59:00 +0400

    Member Layout/Conroller(Destination)

---
+++ Travel/Areas/Member/Controllers/DestinationController.cs
@@ -0,0 +1,16 @@
+﻿using BuisnessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travel.Areas.Member.Controllers
+{
+    [Area("Member")]
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
