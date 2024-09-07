ee84e271019b61bae4bbadaf5a3775e0c561af29
AbdullaArif <arifabdulla034@gmail.com>
2024-09-08 00:35:02 +0400

    Admin Partial Views

--- DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net6.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2999d56c2e780fc2f68a94a5e9abcb750a013443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5971150abde690f77faed745be588282f604b586")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
